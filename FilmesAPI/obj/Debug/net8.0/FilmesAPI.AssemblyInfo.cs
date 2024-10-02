b54b7c605b81b414cdb07d83d4f3a2d35eb8a0f9
Gabrielle Maciel <41557637+gabeesms@users.noreply.github.com>
2024-10-02 19:34:34 -0300

    debug

--- FilmesAPI/obj/Debug/net8.0/FilmesAPI.AssemblyInfo.cs
+++ FilmesAPI/obj/Debug/net8.0/FilmesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c29da7b42cf7341be06c58e5f784128dd6d3e68b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
