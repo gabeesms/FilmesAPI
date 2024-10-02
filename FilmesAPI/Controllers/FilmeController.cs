c29da7b42cf7341be06c58e5f784128dd6d3e68b
Gabrielle Maciel <41557637+gabeesms@users.noreply.github.com>
2024-10-02 16:12:22 -0300

    first commit

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,27 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody]Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+
+    }
+    [HttpGet]
+
+    public List<Filme> RecuperarFilmes()
+    {
+        return filmes;
+    }
+}
